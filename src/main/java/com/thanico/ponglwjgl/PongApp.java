30df683290375c5b94de7bf256a13980a8c20065
ThaNico <ThaNico@users.noreply.github.com>
2020-02-16 23:35:23 +0100

    Disable resizing

--- src/main/java/com/thanico/ponglwjgl/PongApp.java
+++ src/main/java/com/thanico/ponglwjgl/PongApp.java
@@ -87,7 +87,7 @@ public class PongApp {
 		// Configure GLFW
 		glfwDefaultWindowHints(); // optional, the current window hints are already the default
 		glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE); // the window will stay hidden after creation
-		glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE); // the window will be resizable
+		glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE); // the window will be resizable
 
 		// Create the window
 		window = glfwCreateWindow(width, height, applicationName, NULL, NULL);
