b2886a06cbad9035e76d35c63c99f251b7adf2dc
ThaNico <ThaNico@users.noreply.github.com>
2020-02-16 23:24:30 +0100

    add echap key callback

--- src/main/java/com/thanico/ponglwjgl/PongApp.java
+++ src/main/java/com/thanico/ponglwjgl/PongApp.java
@@ -34,7 +34,13 @@ public class PongApp {
 		rightPaddle = new PongPaddle(0.97f, 0.95f);
 
 		glfwSetKeyCallback(window, (window, key, scancode, action, mods) -> {
-			if (action != GLFW_RELEASE) {
+			// Setup a key callback. It will be called every time a key is pressed, repeated
+			// or released.
+			if (key == GLFW_KEY_ESCAPE && action == GLFW_RELEASE) {
+				glfwSetWindowShouldClose(window, true); // We will detect this in the rendering loop
+			}
+			// Action key for paddles
+			else if (action != GLFW_RELEASE) {
 				if (key == GLFW_KEY_Z) {
 					leftPaddle.moveTop(MOVE_FACTOR);
 				} else if (key == GLFW_KEY_S) {
@@ -79,13 +85,6 @@ public class PongApp {
 		window = glfwCreateWindow(300, 300, "Hello World!", NULL, NULL);
 		if (window == NULL)
 			throw new RuntimeException("Failed to create the GLFW window");
-
-		// Setup a key callback. It will be called every time a key is pressed, repeated
-		// or released.
-		glfwSetKeyCallback(window, (window, key, scancode, action, mods) -> {
-			if (key == GLFW_KEY_ESCAPE && action == GLFW_RELEASE)
-				glfwSetWindowShouldClose(window, true); // We will detect this in the rendering loop
-		});
 
 		// Get the thread stack and push a new frame
 		try (MemoryStack stack = stackPush()) {
